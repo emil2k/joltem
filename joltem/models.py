80d8829518bb6aff490d9a0bb4e83f77ceb8746c
Emil <emil2k@gmail.com>
2013-06-07 21:43:47 +0200

    Calculation of impact and completed solutions and gravatar added to user profile.

--- joltem/models.py
+++ joltem/models.py
@@ -3,6 +3,18 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-    impact = models.BigIntegerField(default=1)
+    gravatar_email = models.CharField(max_length=200, null=True,blank=True)
+    gravatar_hash = models.CharField(max_length=200, null=True,blank=True)
     # Relations
     user = models.OneToOneField(User, related_name="profile")
+
+    @property
+    def impact(self):
+        impact = 1  # default to 1 to prevent divide by zero problems
+        for solution in self.user.solution_set.filter(is_accepted=True, is_completed=True):
+            impact += solution.impact
+        return impact
+
+    @property
+    def completed(self):
+        return self.user.solution_set.filter(is_completion_accepted=True).count()
