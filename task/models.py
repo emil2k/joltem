b8d9c76961636424afaa1fe6cc8b4708c0ad8607
Emil <emil2k@gmail.com>
2013-07-06 18:58:42 +0200

    Smaller font on solution rows.

--- task/models.py
+++ task/models.py
@@ -6,6 +6,7 @@ from project.models import Project
 class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
+    # TODO keep time created
     # Relations
     project = models.ForeignKey(Project)
     parent = models.ForeignKey('solution.Solution', null=True, blank=True, related_name="tasks")
