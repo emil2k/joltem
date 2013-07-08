4bcfd38d1bf4f72a534b277b4ba5af2fc0c9bccd
Emil <emil2k@gmail.com>
2013-07-08 14:08:03 +0200

    Moved solutions tabs into solution app.

--- project/urls.py
+++ project/urls.py
@@ -8,9 +8,6 @@ urlpatterns = patterns(
     url(r'^repositories/$', views.repositories, name='repositories'),
     url(r'^repositories/hidden/$', views.repositories_hidden, name='repositories_hidden'),
     url(r'^repositories/new/$', views.new_repository, name='new_repository'),
-    url(r'^solutions/$', views.solutions, name='solutions'),
-    url(r'^solutions/mine/$', views.solutions_my, name='solutions_my'),
-    url(r'^solutions/need-review/$', views.solutions_review, name='solutions_review'),
     url(r'^task/', include('task.urls', namespace='task')),
     url(r'^solution/', include('solution.urls', namespace='solution')),
 )
--- solution/urls.py
+++ solution/urls.py
@@ -7,6 +7,9 @@ urlpatterns = patterns(
     url(r'^(?P<task_id>(\d)+)/new/$', login_required(views.new_solution), name='new_solution'),
     url(r'^(?P<solution_id>(\d)+)/$', views.solution, name='solution'),
     url(r'^(?P<solution_id>(\d)+)/edit/$', views.solution_edit, name='solution_edit'),
-    url(r'^(?P<solution_id>(\d)+)/review/$', views.review, name='review'),
+    url(r'^(?P<solution_id>(\d)+)/review/$', views.review, name='solution_review'),
     url(r'^(?P<solution_id>(\d)+)/commits/(?:(?P<repository_name>[-\w]+)/)?$', views.commits, name='commits'),
-)
+    url(r'^list/$', views.solutions, name='solutions'),
+    url(r'^my/$', views.solutions_my, name='solutions_my'),
+    url(r'^review/$', views.solutions_review, name='solutions_review'),
+    )
