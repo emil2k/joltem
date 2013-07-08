354133bd6893a3e3291744df8e8a3166d67312bf
Emil <emil2k@gmail.com>
2013-07-08 12:48:33 +0200

    Time task was posted.

--- task/models.py
+++ task/models.py
@@ -2,11 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 from project.models import Project
 
+from datetime import datetime
+
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    # TODO keep time created
+    time_posted = models.DateTimeField(default=datetime.now)
     # Relations
     project = models.ForeignKey(Project)
     parent = models.ForeignKey('solution.Solution', null=True, blank=True, related_name="tasks")
