e96fcd7f82c7634f8ebedd31289c8dc2c7b4c810
Emil <emil2k@gmail.com>
2013-07-10 11:51:47 +0200

    Moved project nav up to main nav.
    Also removed old solution page and redirected home page to the unitary project.

--- joltem/urls.py
+++ joltem/urls.py
@@ -11,6 +11,5 @@ urlpatterns = patterns(
     url(r'^keys/$', 'git.views.keys', name='keys'),
     url(r'^in/', views.sign_in, name='sign_in'),
     url(r'^out/', views.sign_out, name='sign_out'),
-    url(r'^solutions/$', views.solutions, name='solutions'),
     url(r'^(?P<project_name>([-\w])+)/', include('project.urls', namespace='project')),
 )
--- joltem/views.py
+++ joltem/views.py
@@ -1,24 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout as auth_logout
 from project.models import Project
-from solution.models import Solution
 
 
 def home(request):
-    context = {
-        'projects': Project.objects.all()
-    }
-    return render(request, 'joltem/home.html', context)
-
-
-def solutions(request):
-    solutions = Solution.objects.filter(user_id=request.user.id)
-    solutions_to_review = Solution.objects.filter(is_completed=True).exclude(user_id=request.user.id)
-    context = {
-        'solutions': solutions,
-        'solutions_to_review': solutions_to_review,
-    }
-    return render(request, 'joltem/solutions.html', context)
+    # Currently there is only one project so just redirect to it
+    project = Project.objects.get()
+    return redirect('project:project', project_name=project.name)
 
 
 def sign_in(request):
