c57b51502332dec8060d286658cb272d8ae93117
Kirill Klenov <horneds@gmail.com>
2013-10-26 14:57:48 +0000

    Remove chefenv from git

--- joltem/settings/chefenv.py
+++
@@ -1,8 +0,0 @@
-from .develop import *
-
-
-DATABASES['default']['HOST'] = "localhost"
-DATABASES['default']['PORT'] = "3306"
-DATABASES['default']['NAME'] = "joltem"
-DATABASES['default']['USER'] = "joltem"
-DATABASES['default']['PASSWORD'] = "joltem"
