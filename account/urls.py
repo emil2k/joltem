dd5ff7b7f2e081a9e1dbc9113a5e9bddf68e6482
marselester <marselester@ya.ru>
2013-11-14 18:44:18 +0700

    Make account urls consistent

--- account/urls.py
+++ account/urls.py
@@ -17,23 +17,23 @@ urlpatterns = patterns(
 
     url(r'^sign-out/$', 'django.contrib.auth.views.logout', name='sign_out'),
 
-    url(r'^password_change/$', 'django.contrib.auth.views.password_change',
+    url(r'^password-change/$', 'django.contrib.auth.views.password_change',
         {'extra_context': {'account_tab': 'change_password'}},
         'password_change'),
 
-    url(r'^password_change/done/$',
+    url(r'^password-change/done/$',
         'django.contrib.auth.views.password_change_done',
         {'extra_context': {'account_tab': 'change_password'}},
         'password_change_done',),
 
-    url(r'^password_reset/$', 'django.contrib.auth.views.password_reset',
+    url(r'^password-reset/$', 'django.contrib.auth.views.password_reset',
         name='password_reset'),
 
-    url(r'^password_reset/done/$',
+    url(r'^password-reset/done/$',
         'django.contrib.auth.views.password_reset_done',
         name='password_reset_done'),
 
-    url(r'^reset/(?P<uidb64>[0-9A-z_\-]+)/(?P<token>[0-9A-z]{1,13}-[0-9A-z]{1,20})/$',
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         'django.contrib.auth.views.password_reset_confirm',
         name='password_reset_confirm'),
 
