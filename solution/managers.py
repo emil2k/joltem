15bbd99ec97f8abd24889c8e943509169d340d29
marselester <marselester@ya.ru>
2013-11-25 20:10:38 +0700

    Add ``need_review_from_user`` QS manager with tests

--- solution/managers.py
+++ solution/managers.py
@@ -7,3 +7,13 @@ class SolutionQuerySet(QuerySet):
     def reviewed_by_user(self, user):
         """Returns solutions which have been voted by given user."""
         return self.filter(vote_set__voter=user)
+
+    def need_review_from_user(self, user):
+        """Returns solutions which have not been voted by given user yet.
+
+        User's solutions are excluded.
+
+        """
+        return self.filter(is_completed=True, is_closed=False) \
+                   .exclude(vote_set__voter=user) \
+                   .exclude(owner_id=user.id)
--- solution/tests/test_managers.py
+++ solution/tests/test_managers.py
@@ -6,7 +6,7 @@ from joltem.libs import mixer
 from ..models import Solution
 
 
-class ReviewedByUserTest(TestCase):
+class SolutionsReviewedByUserTest(TestCase):
 
     def test_get_solution_only_voted_by_given_user_from_two_solutions(self):
         mixer.blend('solution.solution')
@@ -43,3 +43,83 @@ class ReviewedByUserTest(TestCase):
         # "step" parameter of slice syntax is used to evaluate QuerySet.
         with self.assertNumQueries(1):
             Solution.objects.reviewed_by_user(user)[::1]
+
+
+class SolutionsNeedReviewFromUserTest(TestCase):
+
+    def test_solution_has_not_been_reviewed_by_user_yet(self):
+        solution = mixer.blend(
+            'solution.solution',
+            is_completed=True,
+            is_closed=False,
+        )
+
+        bob = mixer.blend('joltem.user')
+        jon = mixer.blend('joltem.user')
+        solution.add_vote(voter=bob, vote_magnitude=1)
+
+        expected_solution = Solution.objects.need_review_from_user(jon).get()
+
+        self.assertEqual(solution, expected_solution)
+
+    def test_empty_qs_when_there_is_no_completed_solution(self):
+        jon = mixer.blend('joltem.user')
+        mixer.blend(
+            'solution.solution',
+            is_completed=False,
+        )
+
+        expected_solution_qty = 0
+
+        self.assertEqual(
+            Solution.objects.need_review_from_user(jon).count(),
+            expected_solution_qty
+        )
+
+    def test_empty_qs_when_there_is_no_open_solution(self):
+        jon = mixer.blend('joltem.user')
+        mixer.blend(
+            'solution.solution',
+            is_completed=True,
+            is_closed=True,
+        )
+
+        expected_solution_qty = 0
+
+        self.assertEqual(
+            Solution.objects.need_review_from_user(jon).count(),
+            expected_solution_qty
+        )
+
+    def test_empty_qs_when_solution_has_already_been_reviewed_by_user(self):
+        solution = mixer.blend(
+            'solution.solution',
+            is_completed=True,
+            is_closed=False,
+        )
+
+        jon = mixer.blend('joltem.user')
+        solution.add_vote(voter=jon, vote_magnitude=1)
+
+        expected_solution_qty = 0
+
+        self.assertEqual(
+            Solution.objects.need_review_from_user(jon).count(),
+            expected_solution_qty
+        )
+
+    def test_empty_qs_when_user_is_owner_of_solution(self):
+        jon = mixer.blend('joltem.user')
+        mixer.blend(
+            'solution.solution',
+            is_completed=True,
+            is_closed=False,
+            owner=jon,
+        )
+
+        expected_solution_qty = 0
+
+        self.assertEqual(
+            Solution.objects.need_review_from_user(jon).count(),
+            expected_solution_qty
+        )
