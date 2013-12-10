06b9accafb24a975c1089be14f721e99519817d1
Emil <emil2k@gmail.com>
2013-12-10 23:02:52 +0100

    Added displaying of comment votes.

--- joltem/holders.py
+++ joltem/holders.py
@@ -11,12 +11,18 @@ class CommentHolder:
         self.comment = comment
         self.url = comment.get_comment_url()
         self.vote = None
+        self.accept_votes = []
+        self.reject_votes = []
+        self.vote_count = 0
         for v in comment.vote_set.all():
+            self.vote_count += 1
             if v.voter_id == user.id:
                 self.vote = v
-                break
+            if v.is_accepted:
+                self.accept_votes.append(v)
+            else:
+                self.reject_votes.append(v)
         self.is_author = user.id == comment.owner_id
-        self.vote_count = len(comment.vote_set.all())
 
     @classmethod
     def get_comments(cls, query_set, user):
