d5b40d5266ecf8ecbdcaca7bb09b5ffbced57e64
Emil <emil2k@gmail.com>
2013-12-10 15:25:42 +0100

    Fixed receiver for updating frozen impact.

--- joltem/receivers.py
+++ joltem/receivers.py
@@ -59,6 +59,7 @@ def update_project_impact_from_voteables(sender, **kwargs):
             user_id=voteable.owner_id
         )
         project_impact.impact = project_impact.get_impact()
+        project_impact.frozen_impact = project_impact.get_frozen_impact()
         project_impact.save()
 
 
