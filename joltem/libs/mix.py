46bdadd4baa780a38d4217ec9465f607c3249130
Kirill Klenov <horneds@gmail.com>
2013-12-31 04:56:35 +0000

    Update authentication key creation.

--- joltem/libs/mix.py
+++ joltem/libs/mix.py
@@ -1,7 +1,7 @@
 """ Prepare mixer to generate Joltem models. """
 from mixer.backend.django import mixer
 
-from git.models import Authentication
+from git.models import Authentication, Repository
 from joltem.models import User
 from project.models import Project
 
@@ -16,7 +16,13 @@ mixer.register(Project, {
     'name': mixer.f.get_simple_username
 })
 
+SSH_TEMPLATE = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCfg+CS9hIcTTI6qRAqfkhS3lzIxlKut25NRr+FIQNTGwHPfDBS3kCATJKKe9MNAO5pqOaWqZ4i5PmHMb+CbfjBxB2IeJR8JqzXocLo8pTTiNoJqWGyGgxZKZK87yLeAAFqthCT2l5QdVoXaygkCK0JZ4Deh63USy4MBWWFLjRHFmE2nVV7vJuoLCEbn7TuiP7M3HQZLTHrZx9liVEIc72d5jdBaNhcthK8oW0rb1zaYCYbBFwvAWVwQNgBC5d7Pgf1XqHKt5j9DlA1pEbHlj8hKNJao6YYSlVlXA8oUuEXoSC/SnguXyP6xsbd9WStPqDh9iMYSqYIu/Ikf7V4%s %s" # noqa
 mixer.register(Authentication, {
-    'key': lambda: 'ssh-rsa %s %s' % (
-        mixer.g.get_string(length=372), mixer.f.get_email()),
+    'key': lambda: SSH_TEMPLATE % (
+        mixer.g.get_string(length=4), mixer.f.get_email()),
+    'name': mixer.f.get_simple_username
+})
+
+mixer.register(Repository, {
+    'name': mixer.mix.project.name,
 })
