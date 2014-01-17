1223bff0b84e3056d907bf2bf0ce594dbb4b1601
Emil <emil2k@gmail.com>
2014-01-17 15:47:47 +0100

    This test fails randomly when first names the same, made it fail consistently until fixed.

--- task/tests/test_notifications.py
+++ task/tests/test_notifications.py
@@ -81,12 +81,12 @@ class TaskNotificationsTest(TestCase):
         self.assertEqual(notify.get_text(), '%s voted on task "%s"' % (
             voter2.first_name, task.title))
 
-        voter3 = mixer.blend('user')
+        voter3 = mixer.blend('user', first_name="Bob")
         task.put_vote(voter3, False)
         self.assertEqual(notify.get_text(), '%s and %s voted on task "%s"' % (
             voter3.first_name, voter2.first_name, task.title))
 
-        voter4 = mixer.blend('user')
+        voter4 = mixer.blend('user', first_name="Bob")
         task.put_vote(voter4, False)
         self.assertEqual(notify.get_text(), '%s, %s, and %s voted on task "%s"' % (
             voter4.first_name, voter3.first_name, voter2.first_name, task.title))
