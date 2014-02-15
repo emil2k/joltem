3b0eb20ef6305536eb30326b2f21d8c76ac263ed
Kirill Klenov <horneds@gmail.com>
2014-02-15 12:58:13 +0000

    Prepare tasks.

--- solution/tasks.py
+++ solution/tasks.py
@@ -20,3 +20,18 @@ def archive_solution(solution):
     """ Make a solution is archived. """
 
     solution.mark_archived()
+
+
+@app.task(ignore_result=True)
+def review_solutions():
+    """ Make a solution is reviewed automatically. """
+    from django.db.models import Count
+    frontier = now() - timedelta(
+        seconds=settings.SOLUTION_REVIEW_PERIOD_SECONDS)
+    for solution in Solution.objects.select_related('owner')\
+            .annotate(num_votes=Count('vote_set'))\
+            .filter(
+                time_completed__lte=frontier,
+                is_completed=True,
+                num_votes=0):
+        solution.mark_close()
--- solution/tests/test_tasks.py
+++ solution/tests/test_tasks.py
@@ -1,14 +1,14 @@
+from django.conf import settings
 from django.test import TestCase
-from joltem.libs import mixer
+from django.utils.timezone import timedelta, now
+
+from joltem.libs import mixer, load_models
 from solution.models import Solution
 
 
 class SolutionTasksTest(TestCase):
 
     def test_archive_solutions(self):
-        from django.utils.timezone import timedelta, now
-        from django.conf import settings
-
         NOW = now()
         frontier = NOW - timedelta(
             seconds=settings.SOLUTION_LIFE_PERIOD_SECONDS + 300)
@@ -35,3 +35,28 @@ class SolutionTasksTest(TestCase):
         notify = s2.owner.notification_set.get()
         self.assertEqual(
             notify.get_text(), 'Solution "%s" was archived' % s2.default_title)
+
+    def test_review_automatically(self):
+        NOW = now()
+        frontier = NOW - timedelta(
+            seconds=settings.SOLUTION_REVIEW_PERIOD_SECONDS + 300)
+
+        s1 = mixer.blend(
+            'solution.solution', task=mixer.RANDOM, time_completed=NOW,
+            is_completed=True)
+        s2 = mixer.blend(
+            'solution.solution', task=mixer.RANDOM, time_completed=frontier,
+            is_completed=True)
+        s3 = mixer.blend(
+            'solution.solution', task=mixer.RANDOM, time_completed=frontier,
+            is_completed=True)
+
+        s3.add_vote(mixer.blend('user'), True)
+
+        from solution.tasks import review_solutions
+        review_solutions.delay()
+
+        s1, s2, s3 = load_models(s1, s2, s3)
+        self.assertFalse(s1.is_closed)
+        self.assertFalse(s3.is_closed)
+        self.assertTrue(s2.is_closed)
