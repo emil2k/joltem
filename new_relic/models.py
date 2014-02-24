56dfeb3014f997f6d4896796b94fcdb165dcf97f
Emil <emil2k@gmail.com>
2014-02-24 14:20:21 +0100

    Added metric calculations.

--- new_relic/models.py
+++ new_relic/models.py
@@ -30,3 +30,73 @@ class NewRelicTransferEvent(models.Model):
         return u'%d in / %d out (bytes) in %d microseconds.' % (
             self.bytes_in, self.bytes_out, self.duration
         )
+
+    @classmethod
+    def get_bytes_in_metrics(cls, qs):
+        """ Returns bytes in metrics for reporting to New Relic.
+
+        :param qs: queryset of transfer events.
+        :return dict:
+
+        """
+        return cls._get_metrics(qs, lambda e: e.bytes_in)
+
+    @classmethod
+    def get_bytes_out_metrics(cls, qs):
+        """ Returns bytes out metrics for reporting to New Relic.
+
+        :param qs: queryset of transfer events.
+        :return dict:
+
+        """
+        return cls._get_metrics(qs, lambda e: e.bytes_out)
+
+    @property
+    def bit_rate(self):
+        """ Bit rate is expressed in Bps ( bytes per second ).
+
+        :return int: Bps
+
+        """
+        bytes = self.bytes_in + self.bytes_out
+        return int(bytes*1000000/float(self.duration))
+
+    @classmethod
+    def get_bit_rate_metrics(cls, qs):
+        """ Returns bit rate metrics for reporting to New Relic.
+
+        Bit rate is expressed in Bps ( bytes per second ).
+
+        :param qs: queryset of transfer events.
+        :return dict:
+
+        """
+        return cls._get_metrics(qs, lambda e: e.bit_rate)
+
+
+    @classmethod
+    def _get_metrics(cls, qs, map):
+        """ Returns metrics for reporting to New Relic.
+
+        :param qs: queryset of transfer events.
+        :param map: function to map event to value.
+        :return dict:
+
+        """
+        metrics = dict(
+            total=0,
+            count=0,
+            min=None,
+            max=None,
+            sum_of_squares=0
+        )
+        for e in qs:
+            value = map(e)
+            metrics['total'] += value
+            metrics['count'] += 1
+            if value < metrics['min'] or metrics['min'] is None:
+                metrics['min'] = value
+            if value > metrics['max'] or metrics['max'] is None:
+                metrics['max'] = value
+            metrics['sum_of_squares'] += pow(value, 2)
+        return metrics
--- new_relic/tests.py
+++ new_relic/tests.py
@@ -22,3 +22,42 @@ class TransferEventTest(TestCase):
                     duration=100000)
         self.assertEqual(load_model(e).__class__.__name__, 'MockTransferEvent')
 
+    def test_bit_rate(self):
+        """ Test Bps calculation. """
+        e = mixer.blend(MockTransferEvent, bytes_in=5244906, bytes_out=176,
+                    duration=254372)
+        self.assertEqual(e.bit_rate, 20619730)
+
+    def test_metrics(self):
+        """ Test calculation of metrics.
+
+        :return:
+
+        """
+        cls = MockTransferEvent
+        mixer.cycle(3).blend(
+            cls,
+            bytes_in=mixer.sequence(5,1,4000*4000),
+            bytes_out=mixer.sequence(7,8,100),
+            duration=mixer.sequence(8,9,5000)
+        )
+        self.assertDictEqual(
+            cls.get_bytes_in_metrics(cls.objects.all()),
+            dict(
+                total=4000*4000+6,
+                count=3,
+                min=1,
+                max=4000*4000,
+                sum_of_squares=25+1+pow(4000*4000,2)
+            )
+        )
+        self.assertDictEqual(
+            cls.get_bytes_out_metrics(cls.objects.all()),
+            dict(
+                total=7+8+100,
+                count=3,
+                min=7,
+                max=100,
+                sum_of_squares=49+64+100*100
+            )
+        )
