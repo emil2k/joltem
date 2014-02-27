b0953f542369ead4643c08192e39347e3f37eaaf
Kirill Klenov <horneds@gmail.com>
2014-02-27 03:40:25 +0000

    Update user admin.

--- joltem/admin.py
+++ joltem/admin.py
@@ -7,11 +7,16 @@ import task.models
 
 from .models import User, Vote, Comment, Notification
 
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = 'username', 'date_joined', 'impact', 'is_active'
+
+
 admin.site.register(Comment)
-admin.site.register(User)
-admin.site.register(Vote)
 admin.site.register(Notification)
-admin.site.register(project.models.Project)
 admin.site.register(project.models.Equity)
 admin.site.register(project.models.Impact)
+admin.site.register(project.models.Project)
 admin.site.register(task.models.Task)
+admin.site.register(User, UserAdmin)
+admin.site.register(Vote)
