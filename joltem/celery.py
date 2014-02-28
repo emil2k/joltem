93a41f742486b5b8a987306471bfe2d37acdb248
Kirill Klenov <horneds@gmail.com>
2014-02-28 04:34:35 +0000

    Clean code

--- joltem/celery.py
+++ joltem/celery.py
@@ -5,11 +5,11 @@ from __future__ import absolute_import
 import os
 
 from celery import Celery
-from django.conf import settings
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'joltem.settings.local')
 
 app = Celery('joltem')
 app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(['joltem','project','solution','gateway'], related_name='tasks')
+app.autodiscover_tasks(['joltem', 'project', 'solution', 'gateway'],
+                       related_name='tasks')
