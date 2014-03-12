d7503fa007996fef832914bb19d15ebc31813a53
Emil <emil2k@gmail.com>
2014-03-12 12:19:59 +0100

    Disable emails on stage.

--- joltem/settings/stage.py
+++ joltem/settings/stage.py
@@ -5,4 +5,7 @@ from .production import *
 
 ENVIRONMENT_NAME = 'stage'
 
+# Disable emails on stage
+EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
+
 logging.info("Stage settings are loaded.")
