c2481085b6dd3f7882cc83a6d14f5d5e0cd4fe27
Kirill Klenov <horneds@gmail.com>
2014-03-15 17:54:43 +0000

    Improve admin interface

--- joltem/admin.py
+++ joltem/admin.py
@@ -3,9 +3,6 @@
 from django.contrib import admin
 from django_markdown.admin import MarkdownModelAdmin
 
-import project.models
-import task.models
-
 from .models import User, Vote, Comment, Notification
 
 
@@ -15,9 +12,5 @@ class UserAdmin(admin.ModelAdmin):
 
 admin.site.register(Comment, MarkdownModelAdmin)
 admin.site.register(Notification)
-admin.site.register(project.models.Equity)
-admin.site.register(project.models.Impact)
-admin.site.register(project.models.Project, MarkdownModelAdmin)
-admin.site.register(task.models.Task, MarkdownModelAdmin)
 admin.site.register(User, UserAdmin)
 admin.site.register(Vote)
